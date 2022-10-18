0a53ca664ddd2eebe327ed6dbc9d3104f2fe335c
Fabien Taxil <fabien.taxil@gmail.com>
2022-10-18 22:40:35 +0100

    tmp

--- src/python_tools/__init__.py
+++ src/python_tools/__init__.py
@@ -1,6 +1,2 @@
 """Random utilities around python."""
-try:
-    from ._version import __version__, __version_tuple__
-except ImportError:
-    __version__ = "0.0.0-UNKNOWN"
-    __version_tuple__ = ("0", "0", "0", "UNKNOWN")
+__version__ = "0.1.0"
