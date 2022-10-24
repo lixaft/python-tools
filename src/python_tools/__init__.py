0a20990a5638727a2672f57048df0761ed984827
Fabien Taxil <lixaft@gmail.com>
2022-10-24 03:15:43 +0100

    Initial commit

---
+++ src/python_tools/__init__.py
@@ -0,0 +1,4 @@
+"""Random utilities around python."""
+from __future__ import annotations
+
+__version__ = "0.1.0"
---
+++ src/python_tools/forget.py
@@ -0,0 +1,22 @@
+"""Completely forget a single module or an entire package."""
+from __future__ import annotations
+
+import sys
+
+
+def forget(name: str, full: bool = True) -> set[str]:
+    """Remove a module from `sys.modules`.
+
+    Arguments:
+        name: The name of the module/package that should be removed.
+        full: Remove every sub-modules of the given name.
+
+    Returns:
+        The list of the forgotten modules.
+    """
+    forgetten = set()
+    for module in reversed(list(sys.modules)):
+        if module == name or (full and module.startswith(name + ".")):
+            forgetten.add(module)
+            del sys.modules[module]
+    return forgetten
